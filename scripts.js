2f3e0eb324cbf5139de44172f6d4daf6fc987f06
Benjamin Nkem <benjaminnkemfrancis@gmail.com>
2025-01-04 21:54:50 +0100

    feat: categories slider section done

--- scripts.js
+++ scripts.js
@@ -22,3 +22,19 @@ function updateCartUI() {
 
   totalPriceElement.textContent = `Total: $${total.toFixed(2)}`;
 }
+
+// CATEGORY SLIDER
+const [leftArrowButton, rightArrowButton] = [
+  document.getElementById("category-slide-left"),
+  document.getElementById("category-slide-right"),
+];
+
+const categoriesContainer = document.querySelector(".category-slide");
+
+leftArrowButton.addEventListener("click", () => {
+  categoriesContainer.scrollLeft -= 300;
+});
+
+rightArrowButton.addEventListener("click", () => {
+  categoriesContainer.scrollLeft += 300;
+});
